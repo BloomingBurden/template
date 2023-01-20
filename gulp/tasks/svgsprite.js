0649c7eaf323d05f566f48fdf5fba3cc05c614fd
Blooming Burden <flowersoverskulls96@gmail.com>
2023-01-20 11:43:18 +0300

    Template 2023 v1.0.4

--- gulp/tasks/svgsprite.js
+++ gulp/tasks/svgsprite.js
@@ -2,18 +2,12 @@ import gulpSvgSprite from 'gulp-svg-sprite';
 
 export function svgSprite() {
     return app.gulp.src(app.path.src.svgicons, {})
-        .pipe(app.plugins.plumber(
-            app.plugins.notify.onError({
-                title: "SVG",
-                message: "Error: <%= error.message %>"
-            })))
         .pipe(gulpSvgSprite({
             mode: {
                 stack: {
                     sprite: `../icons/icons.svg`,
-                    example: true,
                 }
             }
         }))
-        .pipe(app.gulp.dest(app.path.build.js))
+        .pipe(app.gulp.dest(app.path.build.images))
 }
