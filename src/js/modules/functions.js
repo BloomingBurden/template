947849192a12c1ad0fe977e17a66487a8938fb75
Blooming Burden <flowersoverskulls96@gmail.com>
2023-03-01 15:46:12 +0300

    Ready func for repsonsive font-size and debounce function

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -13,4 +13,18 @@ const isWebp = () => {
     });
 };
 
+const debounce = (func, ms) => {
+    let timer = false;
+
+    return function (...arg) {
+        if (timer === true) {
+            return;
+        } 
+        timer = true;
+        func(arg);
+        
+        setTimeout(() => timer = false, ms);
+    }
+}
+
 isWebp();
