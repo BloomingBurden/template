92c40baec44556e7c877e1729fc83583b0dd6916
Blooming Burden <flowersoverskulls96@gmail.com>
2023-03-14 13:14:11 +0300

    Utils.js

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -13,18 +13,4 @@ const isWebp = () => {
     });
 };
 
-const debounce = (func, ms) => {
-    let timer = false;
-
-    return function (...arg) {
-        if (timer === true) {
-            return;
-        } 
-        timer = true;
-        func(arg);
-        
-        setTimeout(() => timer = false, ms);
-    }
-}
-
 isWebp();
